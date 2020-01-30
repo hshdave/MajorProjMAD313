b2d9d8aeb5b1280f973b36ef7136fbd328d20a47
Harsh <hshdave2011@gmail.com>
2020-01-30 17:10:00 -0500

    Fragment NavController

---
+++ app/src/main/java/com/example/myapplication/FirstFragment.java
@@ -0,0 +1,61 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class FirstFragment extends Fragment {
+
+    Button btn_go;
+    private NavController navController;
+
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        navController = Navigation.findNavController(getActivity(),R.id.nav_host_fragment);
+
+        btn_go = view.findViewById(R.id.btn_first);
+
+        btn_go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.secondFragment);
+            }
+        });
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+    }
+
+
+}
